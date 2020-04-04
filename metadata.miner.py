d0a60c8e9a944805ff9c2dfe8f66ea4fbdf7c10c
Akond Rahman <akond.rahman.buet@gmail.com>
2020-04-03 23:45:10 -0500

    EMSE Work: get repo meta data from JSON

--- metadata.miner.py
+++ metadata.miner.py
@@ -5,8 +5,11 @@ Apr 03, 2020
 '''
 import os 
 import json 
+import pandas as pd
 
 def getJSONData(dir_):
+    allContent = []
+    tracker = []
     for root_, dirnames, filenames in os.walk(dir_):
         for file_ in filenames:
             full_path_file = os.path.join(root_, file_) 
@@ -17,7 +20,24 @@ def getJSONData(dir_):
                     if 'items' in json_content:
                         repo_details = json_content['items']
                         for repo_ in repo_details:
-                            print(repo_['full_name'])
+                            url_     = repo_['html_url']
+                            fork     = repo_['fork']
+                            watchers = repo_['watchers_count']
+                            lang     = repo_['language']
+                            issues   = repo_['open_issues']
+                            private  = repo_['private']
+                            date_    = repo_['created_at']
+                            name     = repo_['full_name']
+                            if name not in tracker:
+                                allContent.append( (name, url_, fork, watchers, lang, issues, private, date_) )
+                                tracker.append(name) 
+    df_ = pd.DataFrame(allContent)
+    df_.to_csv('ALL_REPOS.csv', index=False, encoding='utf-8') 
+
+    
+
+
+
 
 if __name__=='__main__':
     json_dir = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/SciSoft/COVID19/dataset/'
